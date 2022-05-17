f6017842dc209ee422a3405e01dd38770f85867e
Duy Tung Ha <44284106+DuyTungHa@users.noreply.github.com>
2022-05-17 11:53:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get -y install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-sudo apt update
-sudo apt-get -y install libmysqlclient-dev
+sudo yum update
+sudo yum -y install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+sudo yum update
+sudo yum -y install libmysqlclient-dev
 pip3 install wheel
-sudo apt -y install python3-venv
+sudo yum -y install python3-venv
 python3 -m venv myprojectenv
 source myprojectenv/bin/activate
-export FLASK_APP=flasky.py
 export FLASK_DEBUG=1
+export FLASK_APP=flasky.py
 pip install -r requirements/dev.txt
-
